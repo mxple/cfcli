1ed43bd337179e2444c7040be7736af3965cb672
mxple <ericalexli@gmail.com>
2024-03-12 15:30:33 -0400

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
-fn main() {
-    let cmd = std::env::args().nth(1).expect("no command given");
+use clap::{Parser, Subcommand};
 
-    println!("cmd: {:?}", cmd);
+#[derive(Parser, Debug)]
+#[command(name = "cfcli", version = "1.0")]
+// #[clap(disable_help_flag = true)]
+struct Cli {
+    #[command(subcommand)]
+    name: Commands,
 }
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    // "clones" a problem or set of problems (contest)
+    Parse {
+        remote: String,
+    },
+    // submits a specified problem or current problem
+    Submit {
+
+    },
+    Version,
+}
+
+#[derive(Debug)]
+struct Problem {
+    contest_id: u32,
+    problem_id: u32,
+}
+
+#[derive(Debug)]
+struct Contest {
+    problems: Vec<Problem>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+  
+    println!("Hello, {:?}!", cli);
+}
+
